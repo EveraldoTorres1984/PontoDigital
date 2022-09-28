70e322af1a48ddd107cf495b8b68902f4a7f0f45
Everaldo <torresjr1984@hotmail.com>
2022-09-28 12:55:50 -0300

    Iniciada as configurações do AdminLTE

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Site;
+use App\Http\Controllers\Admin;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Site\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [Site\HomeController::class, 'index']);
+
+Route::prefix('painel')->group(function () {
+    Route::get('/', [Admin\HomeController::class, 'index'])->name('admin');
+    Route::get('login', [Admin\Auth\LoginController::class, 'index'])->name('login');
+    Route::post('login', [Admin\Auth\LoginController::class, 'authenticate']);
+
+    Route::get('register', [Admin\Auth\RegisterController::class, 'index'])->name('register');
+    Route::post('register', [Admin\Auth\RegisterController::class, 'register']);
+});
